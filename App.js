c9100b3e26b68b0ba186525fdcc92114ba19d96f
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-06-17 20:11:51 +0530

    ADDED: PUT and DELETE

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Button,
   TextInput,
   ToastAndroid,
+  KeyboardAvoidingView,
 } from "react-native";
 
 export default function App() {
@@ -15,7 +16,9 @@ export default function App() {
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
 
-  const fetchTodo = () => {
+  const [showMethod, setShowMethod] = useState(null);
+
+  const fetchUser = () => {
     fetch(`https://reqres.in/api/users/${id}`)
       .then((response) => response.json())
       .then((json) =>
@@ -23,7 +26,7 @@ export default function App() {
       );
   };
 
-  const postTodo = () => {
+  const AddUser = () => {
     fetch("https://reqres.in/api/users", {
       method: "POST",
       body: {
@@ -43,31 +46,152 @@ export default function App() {
       });
   };
 
+  const UpdateUser = () => {
+    fetch("https://reqres.in/api/users/" + id, {
+      method: "PUT",
+      body: {
+        name: name,
+        job: job,
+      },
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        ToastAndroid.show("Updated object", ToastAndroid.SHORT);
+      });
+  };
+
+  const DeleteUser = () => {
+    fetch("https://reqres.in/api/users/" + id, {
+      method: "DELETE",
+    }).then((response) => {
+      console.log(response);
+      ToastAndroid.show("Deleted object", ToastAndroid.SHORT);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>GET method!</Text>
-      <TextInput
-        placeholder="ID"
-        style={styles.input}
-        value={id}
-        onChangeText={setID}
-      />
-      <Button title="Fetch" onPress={fetchTodo} color="#841584" />
-      <Text>Name: {getName}</Text>
-      <Text style={styles.header}>POST method!</Text>
-      <TextInput
-        placeholder="Name"
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        placeholder="Job"
-        style={styles.input}
-        value={job}
-        onChangeText={setJob}
-      />
-      <Button title="Post" onPress={postTodo} color="#841584" />
+      {showMethod === "GET" && (
+        <View style={styles.methodContainer}>
+          <Text style={styles.header}>GET method!</Text>
+          <TextInput
+            placeholder="ID"
+            style={styles.input}
+            value={id}
+            onChangeText={setID}
+          />
+          <Button
+            title="Fetch"
+            style={styles.button}
+            onPress={fetchUser}
+            color="#6EB4D5"
+          />
+          <Text>Name: {getName}</Text>
+        </View>
+      )}
+      {showMethod === "POST" && (
+        <View style={styles.methodContainer}>
+          <Text style={styles.header}>POST method!</Text>
+          <TextInput
+            placeholder="Name"
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            placeholder="Job"
+            style={styles.input}
+            value={job}
+            onChangeText={setJob}
+          />
+          <Button
+            title="Post"
+            style={styles.button}
+            onPress={AddUser}
+            color="#6EB4D5"
+          />
+        </View>
+      )}
+
+      {showMethod === "PUT" && (
+        <View style={styles.methodContainer}>
+          <Text style={styles.header}>PUT method!</Text>
+          <TextInput
+            placeholder="Id"
+            style={styles.input}
+            value={id}
+            onChangeText={setID}
+          />
+          <TextInput
+            placeholder="Name"
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            placeholder="Job"
+            style={styles.input}
+            value={job}
+            onChangeText={setJob}
+          />
+          <Button
+            title="Post"
+            style={styles.button}
+            onPress={UpdateUser}
+            color="#6EB4D5"
+          />
+        </View>
+      )}
+      {showMethod === "DELETE" && (
+        <View style={styles.methodContainer}>
+          <Text style={styles.header}>DELETE method!</Text>
+          <TextInput
+            placeholder="Id"
+            style={styles.input}
+            value={id}
+            onChangeText={setID}
+          />
+          <Button
+            title="Post"
+            style={styles.button}
+            onPress={DeleteUser}
+            color="#6EB4D5"
+          />
+        </View>
+      )}
+      <View style={styles.optionsButton}>
+        <Button
+          title="GET"
+          style={styles.button}
+          onPress={() => setShowMethod("GET")}
+          color="#6EB4D5"
+        />
+
+        <Button
+          title="POST"
+          style={styles.button}
+          onPress={() => setShowMethod("POST")}
+          color="#6EB4D5"
+        />
+
+        <Button
+          title="PUT"
+          style={styles.button}
+          onPress={() => setShowMethod("PUT")}
+          color="#6EB4D5"
+        />
+
+        <Button
+          title="DELETE"
+          style={styles.button}
+          onPress={() => setShowMethod("DELETE")}
+          color="#6EB4D5"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -75,9 +199,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   header: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: "bold",
-    color: "blue",
+    color: "#335EA1",
   },
   container: {
     flex: 1,
@@ -86,13 +210,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   button: {
-    margin: 10,
+    height: 40,
   },
   input: {
     borderWidth: 2,
-    width: 200,
+    width: 300,
     margin: 10,
     borderStyle: "solid",
     borderColor: "black",
+    fontSize: 20,
+    padding: 10,
+  },
+
+  optionsButton: {
+    flexDirection: "row",
+    gap: 10,
+    position: "absolute",
+    bottom: 60,
+  },
+  methodContainer: {
+    position: "absolute",
+    top: 70,
   },
 });
