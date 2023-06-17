40bece6bb6a2f6d160d90b6e56988915779bc5f2
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-06-17 17:55:32 +0530

    GET method

--- App.js
+++ App.js
@@ -5,15 +5,11 @@ import {
   Text,
   View,
   Button,
-  TextInput,
-  ToastAndroid,
 } from "react-native";
 
 export default function App() {
   const [todo, setTodo] = useState("");
   const [id, setID] = useState(0);
-  const [name, setName] = useState("");
-  const [job, setJob] = useState("");
 
   // useEffect(async () => {
   //   await fetch("https://jsonplaceholder.typicode.com/todos/1")
@@ -27,47 +23,12 @@ export default function App() {
       .then((json) => setTodo(json.title));
   };
 
-  const postTodo = () => {
-    fetch("https://reqres.in/api/users", {
-      method: "POST",
-      body: {
-        name: name,
-        job: job,
-      },
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setID(json.id);
-        ToastAndroid.show(
-          json.id + " " + json.name + " " + json.job,
-          ToastAndroid.SHORT
-        );
-      });
-  };
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Button title="Fetch" onPress={fetchTodo} color="#841584" />
       <Text style={styles.button}>{todo}</Text>
       <Text>{id}</Text>
-      <TextInput
-        placeholder="Name"
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-      />
-      <TextInput
-        placeholder="Job"
-        style={styles.input}
-        value={job}
-        onChangeText={setJob}
-      />
-      <Button title="Post" onPress={postTodo} color="#841584" />
       <StatusBar style="auto" />
     </View>
   );
